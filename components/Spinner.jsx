9b10513f7d2dc1225de5ea022d6d04fd19e8fa72
rabiu <medrabiu@gmail.com>
2024-07-14 13:00:53 +0100

    new

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -2,7 +2,7 @@
 
 const Spinner = () => (
     <div className="flex items-center justify-center h-screen">
-      <img src="/tsbot.jpg" alt="Loading..." className="rounded-full w-20 h-20 animate-spin" />
+      <img src="/coin.webp" alt="Loading..." className="rounded-full w-20 h-20 animate-spin" />
     </div>
   );
   
